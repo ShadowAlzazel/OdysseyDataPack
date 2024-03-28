f752ff63cf57b93947bea5a6f34425817d72fc93
ShadowAlzazel <pastenalfredojr@gmail.com>
2024-03-27 19:01:03 -0700

    Recipe creater basice version

--- OdysseyDataPack/data/odyssey/recipes/weapons/_gen_weapon_recipes.py
+++ OdysseyDataPack/data/odyssey/recipes/weapons/_gen_weapon_recipes.py
@@ -1,6 +1,8 @@
 import os 
 import json
 
+from _weapon_data import *
+
 abspath = os.path.abspath(__file__)
 dir_name = os.path.dirname(abspath)
 os.chdir(dir_name)
@@ -9,8 +11,55 @@ os.chdir(dir_name)
 # Add [Polexe], [Battleaxe(labrys)]
 # Rework [Rapier], [Lance]
 
-tool_types = ['chakram', 'claymore', 'cutlass', 'dagger', 'halberd', 'katana', 'kunai', 'lance', 
-                'longaxe', 'longsword', 'rapier', 'saber', 'scythe', 'sickle', 'spear', 'warhammer']
-materials = ['wooden', 'golden', 'stone', 'iron', 'diamond', 'netherite',
-             'copper', 'silver', 'soul_steel', 'titanium', 'andonized_titanium', 'iridium', 'mithril']
+#tool_types = ['chakram', 'claymore', 'cutlass', 'dagger', 'halberd', 'katana', 'kunai', 'lance', 
+#                'longaxe', 'longsword', 'rapier', 'saber', 'scythe', 'sickle', 'spear', 'warhammer']
+#materials = ['wooden', 'golden', 'stone', 'iron', 'diamond', 'netherite',
+#             'copper', 'silver', 'soul_steel', 'titanium', 'andonized_titanium', 'iridium', 'mithril']
 
+# Function to create weapon recipe files 
+def create_weapon_recipe_files(): 
+    for i in range(len(materials)):
+        for j in range(len(tool_types)):
+            mat: Material = materials[i]
+            ttyp: ToolType = tool_types[j]
+            # Create variables for names
+            item_name = f'{mat.name}_{ttyp.name}'
+            custom_name = f'{mat.material_name} {ttyp.type_name}'
+            filename = f'{item_name}.json'
+            # Create Recipe keys and objects
+            key = material_keys[mat.name]
+            pattern = tool_patterns[ttyp.name]
+            # Create json obj to make interpolation easier
+            json_obj = {
+                "type": "minecraft:crafting_shaped",
+                "category": "misc",
+                "group": f"{ttyp.name}",
+                "key": key,
+                "pattern": pattern,
+                "result": {
+                    "count": 1,
+                    "id": f"minecraft:{mat.item_override_pre}_{ttyp.item_override_suf}",
+                    "components": {
+                        "minecraft:custom_model_data": (mat.item_model_pre * 100) + (ttyp.item_model_suf),
+                        "minecraft:item_name": f"{item_name}",
+                        "minecraft:custom_name": f"{custom_name}"
+                    }
+                },
+                "show_notification": False
+            }
+            text = json.dumps(json_obj, indent=2)
+            # Write the text to opened file
+            with open(filename, 'w') as file:
+                file.write(text)
+            
+
+# Prompt 
+print("Confirm Creation of New files? This will overwrite old files.")
+print(f"Will Create {len(materials) * len(tool_types)} tool combinations.")
+print("Proceed (y/n) . . .")
+answer = input()
+
+# Input
+if answer == "y":
+    print("Ok")
+    create_weapon_recipe_files() 
--- OdysseyDataPack/data/odyssey/recipes/weapons/_weapon_data.py
+++ OdysseyDataPack/data/odyssey/recipes/weapons/_weapon_data.py
@@ -1,6 +1,6 @@
 import os 
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 abspath = os.path.abspath(__file__)
 dir_name = os.path.dirname(abspath)
@@ -9,12 +9,60 @@ os.chdir(dir_name)
 @dataclass(frozen=True, order=True)
 class Material: 
     name: str = 'material' # 'item_name' component
-    custom_name: str = 'Custom Material' # 'custon_name' component
-    base_damage: int = 0
+    material_name: str = 'Custom Material' # 'custom_name' component
+    mat_damage: int = 0
+    item_model_pre: int = 12345 # 'custom_model_data' compnent [12345XX]
+    item_override_pre: str = 'diamond' # for finding the item id
     
     
 @dataclass(frozen=True, order=True)
-class WeaponType: 
-    name: str = 'weapon'
-    custom_name: str = 'Custom Weapon'
+class ToolType: 
+    name: str = 'weapon'  # 'item_name' component
+    type_name: str = 'Custom Tool' # 'custom_name' component
     base_damage: int = 0
+    base_speed: int = 1.0
+    item_model_suf: int = 67 # 'custom_model_data' compnent [XXXXX67]
+    item_override_suf: str = 'sword' # for finding the item id
+    #pattern: list[str] = field(default_factory=list)
+
+materials = [
+    Material('iron', 'Iron', 3.0, 69057, 'iron'),
+    Material('diamond', 'Diamond', 4.0, 69057, 'diamond')
+    #Material('titanium', 'Titanium', 4.0, 69068),
+    #Material('andonized_titanium', 'Andonized Titanium', 4.0, 69070)
+]
+    
+tool_types = [
+    ToolType('katana', 'Katana', 4.0, 1.5, 44, 'sword'),
+    ToolType('claymore', 'Claymore', 7.0, 0.85, 45, 'sword')
+]
+
+
+tool_patterns = {
+    'katana': [
+        "  X",
+        "CX ",
+        "|  "
+    ],
+    'claymore': [
+        " X ",
+        "XXX",
+        " | "
+    ]
+} 
+
+# Add way to ovveride 
+material_keys = {
+    'golden': {
+        "X": {"item": "minecraft:gold_ingot"},
+        "|": {"item": "minecraft:stick"}
+    },
+    'iron': {
+        "X": {"item": "minecraft:iron_ingot"},
+        "|": {"item": "minecraft:stick"}
+    },
+    'diamond': {
+        "X": {"item": "minecraft:diamond"},
+        "|": {"item": "minecraft:stick"}
+    }
+}
